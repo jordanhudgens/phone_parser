a054bdc3737e04fa53330a911a855b8ed24a5776
Jordan Hudgens <jordan@wow.com>
2017-04-21 09:57:22 -0700

    Added phone parsing implementation and specs

--- lib/phone_parser.rb
+++ lib/phone_parser.rb
@@ -1,5 +1,43 @@
 require "phone_parser/version"
+require "phone_parser/country_codes"
 
 module PhoneParser
-  # Your code goes here...
+  include CountryCodes
+
+  def self.parse(number)
+    number.delete!("^0-9")
+    digit_length_validator(number)
+    country_code_validator(number)
+  end
+
+  def self.digit_length_validator number
+    number.length < 10 ? (raise PhoneLengthError) : (number)
+  end
+
+  def self.country_code_validator number
+    if number.length == 10
+      number
+    else
+      extracted_number = number[-10..-1]
+      country_code = number.gsub(extracted_number, '')
+
+      country_codes.include?(country_code) ? (number) : (raise CountryCodeError)
+    end
+  end
+
+  def self.country_codes
+    CountryCodes.code_array.flatten.keep_if { |e| e =~ /\d+/ }
+  end
+end
+
+class PhoneLengthError < StandardError
+  def message
+    'Phone numbers need to have at least 10 digits'
+  end
+end
+
+class CountryCodeError < StandardError
+  def message
+    'Country code not recognized'
+  end
 end
